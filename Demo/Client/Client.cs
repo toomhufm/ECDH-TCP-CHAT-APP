7ee1a26b910ab367a08f0cfbd64dc481ee3a6b17
toomhufm <21521964@gm.uit.edu.vn>
2023-05-17 19:14:09 +0700

    Upload Project

---
+++ Demo/Client/Client.cs
@@ -0,0 +1,241 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Data;
+using System.Text;
+using System.Security.Cryptography;
+
+namespace Client
+{
+    class Program
+    {
+        private static byte[] PublicKey;
+        static ECDiffieHellmanCng ecc = new ECDiffieHellmanCng(ECCurve.NamedCurves.nistP256);
+
+        static readonly Dictionary<byte[], string> friend_pk = new Dictionary<byte[], string>();
+
+        static void Help()
+        {
+            Console.WriteLine("========================== HELP MENU ===============================");
+            Console.WriteLine("Use /share to broadcast your Public Key");
+            Console.WriteLine("Use /p <friend-name> | <message> to chat in private with your friend");
+            Console.WriteLine("Use /o to see who is online");
+            Console.WriteLine("====================================================================");
+        }
+
+        static void Banner()
+        {
+            string banner = @"Use /h for available command";
+            Console.WriteLine(banner);
+        }
+        static void Main(string[] args)
+        {
+            Banner();
+            KeyGen();
+            Console.WriteLine($"Your Public Key : {Convert.ToHexString(PublicKey)}");
+            IPAddress ip = IPAddress.Parse("127.0.0.1");
+            int port = 5000;
+            TcpClient client = new TcpClient();
+            client.Connect(ip, port);
+            Console.Write("Enter your name : ");
+            string username = Console.ReadLine();
+            NetworkStream ns = client.GetStream();
+            ns.Write(Encoding.UTF8.GetBytes($"@username|{username}"));
+            ns.Flush();
+            Console.WriteLine("You connected!!");
+            Thread thread = new Thread(o => ReceiveData((TcpClient)o));
+            thread.Start(client);
+            string s;
+            while (!string.IsNullOrEmpty((s = Console.ReadLine())))
+            {
+
+                if (s.StartsWith("/p"))
+                {
+                    try
+                    {
+                        string[] msg = s.Split('|');
+                        string friend_name = msg[0].Split(' ')[1].TrimEnd();
+                        byte[] enc = null;
+                        EncryptMessage(msg[1], friend_name, out enc);
+                        string hex_enc = Convert.ToHexString(enc);
+                        string private_msg = $"@private-{friend_name}|{hex_enc}|{friend_name}";
+                        //Console.WriteLine(private_msg);
+                        byte[] buffer = Encoding.ASCII.GetBytes(private_msg);
+                        ns.Write(buffer, 0, buffer.Length);
+
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.ToString());
+                    }
+
+                }
+                else if (s.StartsWith("/h"))
+                {
+                    Help();
+                }
+
+                else if (s.StartsWith("/share"))
+                {
+                    BroadCastPublicKey(PublicKey,client);
+                    Console.WriteLine("[NOTI] Shared your public key");
+                }
+                else if (s.StartsWith("/o"))
+                {
+                    ShowFriend();
+                }
+
+                else
+                {
+                    byte[] buffer = Encoding.ASCII.GetBytes($"#{username}|{s}");
+                    ns.Write(buffer, 0, buffer.Length);
+                }
+            }
+
+            client.Client.Shutdown(SocketShutdown.Send);
+            thread.Join();
+            ns.Close();
+            client.Close();
+            Console.WriteLine("disconnect from server!!");
+            Console.ReadKey();
+        }
+
+        static void ReceiveData(TcpClient client)
+        {
+            NetworkStream ns = client.GetStream();
+            byte[] receivedBytes = new byte[2048];
+            int byte_count;
+            while ((byte_count = ns.Read(receivedBytes, 0, receivedBytes.Length)) > 0)
+            {
+                string msg = (Encoding.ASCII.GetString(receivedBytes, 0, byte_count));
+                if (msg.Contains("connected"))
+                {
+                    Console.WriteLine($"[New Comer] : {msg}");
+                }
+                else if (msg.StartsWith("@publickey"))
+                {
+                    string[] split = msg.Split('|');
+                    byte[] fpk = ToByteArray(split[1]);
+                    string friend_name = split[2];
+                    friend_pk.Add(fpk, friend_name.TrimEnd());
+                }
+                else if (msg.StartsWith("@private"))
+                {
+                    try
+                    {
+                        string[] split = msg.Split('|');
+                        string friend_name = split[2].Trim();
+                        string message = split[1].Trim();
+                        byte[] cipher = ToByteArray(message);
+                        DecryptMessage(cipher, friend_name);
+
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);  
+                    }
+                }
+                else
+                {
+                    string[] msg_split = msg.Split('|');
+                    Console.WriteLine($"[GLOBAL][FROM {msg_split[0]} ] : {msg_split[1]}");
+                }
+            }
+        }
+
+
+        static void EncryptMessage(string msg, string receiver, out byte[] encrypted_msg)
+        {
+
+            using (Aes aes = new AesCryptoServiceProvider())
+            {
+                byte[] _keyBlob = GetFriendKey(receiver);
+                CngKey receiver_pk = CngKey.Import(_keyBlob, CngKeyBlobFormat.EccPublicBlob);
+                byte[] shared_secret = ecc.DeriveKeyMaterial(receiver_pk);
+
+                aes.Key = shared_secret;
+                aes.IV = Encoding.ASCII.GetBytes("bQeThWmZq4t6w9z$");
+
+                // Encrypt the message
+                using (MemoryStream ciphertext = new MemoryStream())
+                using (CryptoStream cs = new CryptoStream(ciphertext, aes.CreateEncryptor(), CryptoStreamMode.Write))
+                {
+                    byte[] plaintextMessage = Encoding.UTF8.GetBytes(msg);
+                    cs.Write(plaintextMessage, 0, plaintextMessage.Length);
+                    cs.Close();
+                    encrypted_msg = ciphertext.ToArray();
+                }
+            }
+        }
+
+        static void DecryptMessage(byte[] ciphertext, string sender)
+        {
+            using (Aes aes = new AesCryptoServiceProvider())
+            {
+                byte[] _keyBlob = GetFriendKey(sender);
+                CngKey sender_pk = CngKey.Import(_keyBlob, CngKeyBlobFormat.EccPublicBlob);
+                byte[] shared_secret = ecc.DeriveKeyMaterial(sender_pk);
+                aes.Key = shared_secret;
+                aes.IV = Encoding.ASCII.GetBytes("bQeThWmZq4t6w9z$");
+                // Decrypt the message
+                using (MemoryStream plaintext = new MemoryStream())
+                {
+                    using (CryptoStream cs = new CryptoStream(plaintext, aes.CreateDecryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(ciphertext, 0, ciphertext.Length);
+                        cs.Close();
+                        string message = Encoding.UTF8.GetString(plaintext.ToArray());
+                        Console.WriteLine($"[PRIVATE][FROM {sender}] : {message}");
+                    }
+                }
+            }
+        }
+
+        static void KeyGen()
+        {
+            ecc.HashAlgorithm = CngAlgorithm.Sha256;
+            ecc.KeyDerivationFunction = ECDiffieHellmanKeyDerivationFunction.Hash;
+            PublicKey = ecc.PublicKey.ToByteArray();
+        }
+
+        static void BroadCastPublicKey(byte[] PublicKey, TcpClient self)
+        {
+            NetworkStream ns = self.GetStream();
+            string msg = $"@publickey|{Convert.ToHexString(PublicKey)}";
+            byte[] buffer = Encoding.ASCII.GetBytes(msg);
+            ns.Write(buffer, 0, buffer.Length);
+        }
+        static byte[] ToByteArray(String hexString)
+        {
+            byte[] retval = new byte[hexString.Length / 2];
+            for (int i = 0; i < hexString.Length; i += 2)
+                retval[i / 2] = Convert.ToByte(hexString.Substring(i, 2), 16);
+            return retval;
+        }
+
+        static void ShowFriend()
+        {
+            Console.WriteLine("Online list : ");
+            foreach(byte[] pk in friend_pk.Keys)
+            {
+                Console.WriteLine(friend_pk[pk]);
+                Console.WriteLine(Convert.ToHexString(GetFriendKey(friend_pk[pk])));
+            }
+        }
+
+        static byte[] GetFriendKey(string name)
+        {
+            name.TrimEnd();
+            foreach (byte[] pk in friend_pk.Keys)
+            {
+                if (friend_pk[pk].Contains(name))
+                {
+                    //Console.WriteLine($"Found {name} Public Key ");
+                    return pk;
+                }
+            }
+            Console.WriteLine("Public Key Not Found");
+            return null;
+        }
+    }
+}
---
+++ Demo/Demo/Server.cs
@@ -0,0 +1,142 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Data;
+using System.Text;
+using System.Security.Cryptography;
+
+namespace Server
+{
+
+    class Program
+    {
+        private static void Banner()
+        {
+            string banner = @"
+            =========================================================================
+            =                            ECDH DEMOSTRATION                          =
+            =========================================================================
+            ";
+            Console.WriteLine(banner);
+        }
+
+        static readonly object _lock = new object();
+        static readonly Dictionary<int, TcpClient> list_clients = new Dictionary<int, TcpClient>();
+        static readonly Dictionary<TcpClient,string> clients_name = new Dictionary<TcpClient,string>();
+        static readonly Dictionary<TcpClient,byte[]> clients_pk = new Dictionary<TcpClient,byte[]>();
+        static void Main(string[] args)
+        {
+            int count = 1;
+            Banner();
+            TcpListener ServerSocket = new TcpListener(IPAddress.Any, 5000);
+            ServerSocket.Start();
+
+            while (true)
+            {
+                TcpClient client = ServerSocket.AcceptTcpClient();
+                lock (_lock) list_clients.Add(count, client);
+
+                Thread t = new Thread(handle_clients);
+                t.Start(count);
+                count++;
+            }
+        }
+
+        public static void handle_clients(object o)
+        {
+            int id = (int)o;
+            TcpClient client;
+
+            lock (_lock) client = list_clients[id];
+            System.Net.ServicePointManager.Expect100Continue = false;
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+            while (true)
+            {
+                NetworkStream stream = client.GetStream();
+                byte[] buffer = new byte[1024];
+                int byte_count = stream.Read(buffer, 0, buffer.Length);
+
+                if (byte_count == 0)
+                {
+                    break;
+                }
+
+                string data = Encoding.ASCII.GetString(buffer, 0, byte_count);
+                if (data.StartsWith("@username"))
+                {
+                    string username = data.Split('|')[1];
+                    clients_name.Add(client, username);
+                    broadcast($"{username} connected", client);
+                    Console.WriteLine($"{username} connected");
+                }
+
+                else if (data.StartsWith("@private"))
+                {
+                    string[] split = data.Split('|');
+                    string name = split[2];
+                    string msg = split[1];
+                    privateChat(msg,name,client);
+                }
+
+                else if (data.StartsWith("@publickey"))
+                {
+                    string pk = data.Split('|')[1];
+                    byte[] PublicKey = ToByteArray(pk);
+                    clients_pk.Add(client, PublicKey);
+                    broadcast(data + $"|{clients_name[client]}", client);
+                }
+                else
+                {
+                    broadcast(data,client);
+                    Console.WriteLine(data);
+
+                }
+            }
+
+            lock (_lock) list_clients.Remove(id);
+            client.Client.Shutdown(SocketShutdown.Both);
+            client.Close();
+        }
+
+        public static void broadcast(string data, Object client)
+        {
+            byte[] buffer = Encoding.ASCII.GetBytes(data + Environment.NewLine);
+
+            lock (_lock)
+            {
+                foreach (TcpClient c in list_clients.Values)
+                {
+                    if(c != client)
+                    {
+                        NetworkStream stream = c.GetStream();
+
+                        stream.Write(buffer, 0, buffer.Length);
+                    }
+                }
+            }
+        }
+
+        public static void privateChat(string msg, string receiver_name, TcpClient sender)
+        {
+            string sender_name = clients_name[sender];
+            msg = $"@private|{msg}|{sender_name}";
+            foreach(TcpClient c in clients_name.Keys)
+            {
+                if(clients_name[c] == receiver_name)
+                {
+                    byte[] buffer = Encoding.ASCII.GetBytes(msg);
+                    NetworkStream stream = c.GetStream();
+                    stream.Write(buffer, 0, buffer.Length);
+                    Console.WriteLine(msg);
+                }
+            }
+        }
+        public static byte[] ToByteArray(String hexString)
+        {
+            byte[] retval = new byte[hexString.Length / 2];
+            for (int i = 0; i < hexString.Length; i += 2)
+                retval[i / 2] = Convert.ToByte(hexString.Substring(i, 2), 16);
+            return retval;
+        }
+    }
+}
